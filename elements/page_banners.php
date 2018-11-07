c7df0d65e0541b60d9287c854aee3897b0809a38
Paul Barthmaier <paul@barthmaier.rocks>
2018-11-06 19:10:32 -0500

    new branch to do PR
    added a space to be able to save something innocuous

--- elements/page_banners.php
+++ elements/page_banners.php
@@ -15,7 +15,7 @@ function memberlite_elements_admin_enqueue_scripts_for_page_banners() {
 }
 add_action( 'admin_enqueue_scripts', 'memberlite_elements_admin_enqueue_scripts_for_page_banners' );
 
-/* Adds a Memberlite settings meta box to the side column on the Page edit screens. */
+/* Adds a Memberlite settings meta box to the side column on the Page edit screens.  */
 function memberlite_elements_settings_add_meta_box() {
 	$screens = array( 'page' );
 	foreach ( $screens as $screen ) {
