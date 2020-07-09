ab99d6daf576d6997fc5d8e78251b539ef9e7882
Ronald Huereca <rhuereca@updraftplus.com>
2020-07-09 11:44:43 -0500

    Registering memberlite scripts and assets.

--- blocks/init.php
+++ blocks/init.php
@@ -25,3 +25,29 @@ function memberlite_add_block_category( $categories, $post ) {
 	);
 }
 add_filter( 'block_categories', 'memberlite_add_block_category', 10, 2 );
+
+/**
+ * Register admin scripts and styles.
+ */
+function memberlite_register_block_scripts_admin() {
+	wp_register_script(
+		'memberlite_elements_blocks',
+		MEMBERLITE_ELEMENTS_URL . '/js/blocks.build.js',
+		array(
+			'wp-blocks',
+			'wp-element',
+		),
+		MEMBERLITE_ELEMENTS_VERSION,
+		true
+	);
+	wp_set_script_translations( 'memberlite_elements_blocks', 'memberlite-elements' );
+}
+add_action( 'wp_enqueue_scripts', 'memberlite_register_block_scripts_admin' );
+
+/**
+ * Register front-end block scripts and styles.
+ */
+function memberlite_register_block_scripts_front_end() {
+
+}
+add_action( 'wp_enqueue_scripts', 'memberlite_register_block_scripts_front_end' );
