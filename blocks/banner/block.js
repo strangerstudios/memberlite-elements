b9392c01ec87ccab2f655f2ed9cc940d8c7912ce
Ronald Huereca <rhuereca@updraftplus.com>
2020-07-09 13:26:41 -0500

    Simple hello world output for block on the back-end.

---
+++ blocks/banner/block.js
@@ -0,0 +1,70 @@
+const { Component, Fragment } = wp.element;
+
+const { __, _n } = wp.i18n;
+
+const {
+	PanelBody,
+	Placeholder,
+	RangeControl,
+	SelectControl,
+	TextControl,
+	ToggleControl,
+	Button,
+} = wp.components;
+
+const {
+	InspectorControls,
+	PanelColorSettings,
+} = wp.blockEditor;
+
+class MemberliteElementsBanner extends Component {
+	constructor() {
+		super(...arguments);
+
+		// Set initial state here if needed.
+	}
+
+	render = () => {
+		const { attributes, setAttributes } = this.props;
+
+		const { backgroundColor } = attributes;
+
+		const inspectorControls = (
+			<InspectorControls>
+				<PanelBody
+					initialOpen={true}
+					title={__("Background Color", "memberlite-elements")}
+				>
+						<PanelColorSettings
+							title={__("Background Color", "post-type-archive-mapping")}
+							initialOpen={true}
+							colorSettings={[
+								{
+									value: backgroundColor,
+									onChange: (value) => {
+										setAttributes({ backgroundColor: value });
+									},
+									label: __("Background Color", "memberlite-elements"),
+								},
+							]}
+						></PanelColorSettings>
+					</PanelBody>
+			</InspectorControls>
+		);
+
+		return (
+			<Fragment>
+				{inspectorControls}
+				<Placeholder>
+					<div className="memberlite-block-admin-banner">
+						<h1>
+							Hello World
+						</h1>
+					</div>
+				</Placeholder>
+			</Fragment>
+		);
+	}
+}
+
+export default MemberliteElementsBanner;
